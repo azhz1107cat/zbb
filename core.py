a6b4a21528fae91e7ee43a3fa42d88e87a2f4cab
azhz1107cat <azhz1107cat@outlook.com>
2025-05-14 20:41:37 +0800

    现已基本实现所有功能

--- core.py
+++ core.py
@@ -69,96 +69,97 @@ def main(zbb_file_to_run):
     """
 
     with open(zbb_file_to_run, 'r', encoding='utf-8') as f:
-        public_var.line_num = 0
         code_text = f.read()
-        os.system('chcp 65001')
-        print("==running==\n")
-        start_time = time.time()
-        code_text_to_list = code_text.split('\n')
 
-        """
-        标签、函数、字典预定义
-        """
-        temp_dic_ = {}
-        dic_not_end = ""
-        func_not_end = ""
-        for each_line in code_text_to_list:
-            if each_line.replace(" ","").startswith("::"):
-                label = each_line.replace("::", "",1).replace(" ", "")
-                zbb_mark_dic[label] = public_var.line_num
+    public_var.line_num = 0
+    os.system('chcp 65001')
+    print("==running==\n")
+    start_time = time.time()
+    code_text_to_list = code_text.split('\n')
 
-            if each_line.startswith("func") or each_line.startswith("FUNC"):
-                func_keyword = "func" if each_line.startswith("func") else "FUNC"
-                func_name = each_line.replace(func_keyword, '',1).replace(" ", '')
-                func_not_end = func_name
-                zbb_func_dic[func_name] = {"start": public_var.line_num,
-                                           "end": None,
-                                           "call_from": None
-                                           }
-            if each_line.replace(" ","").startswith("dic") or each_line.startswith("DIC"):
-                dic_keyword = "dic" if each_line.startswith("dic") else "DIC"
-                dic_name = each_line.replace(dic_keyword, '',1).replace(" ", '')
-                dic_not_end = dic_name
+    """
+    标签、函数、字典预定义
+    """
+    temp_dic_ = {}
+    dic_not_end = ""
+    func_not_end = ""
+    for each_line in code_text_to_list:
+        if each_line.replace(" ", "").startswith("::"):
+            label = each_line.replace("::", "", 1).replace(" ", "")
+            zbb_mark_dic[label] = public_var.line_num
 
-            if each_line.replace(" ","").startswith("end") or each_line.startswith("END"):
-                if dic_not_end != "":
-                    zbb_var_dic[dic_not_end] = temp_dic_
-                    temp_dic_ = {}
-                    dic_not_end = ""
+        if each_line.startswith("func") or each_line.startswith("FUNC"):
+            func_keyword = "func" if each_line.startswith("func") else "FUNC"
+            func_name = each_line.replace(func_keyword, '', 1).replace(" ", '')
+            func_not_end = func_name
+            zbb_func_dic[func_name] = {"start": public_var.line_num,
+                                       "end": None,
+                                       "call_from": None
+                                       }
+        if each_line.replace(" ", "").startswith("dic") or each_line.startswith("DIC"):
+            dic_keyword = "dic" if each_line.startswith("dic") else "DIC"
+            dic_name = each_line.replace(dic_keyword, '', 1).replace(" ", '')
+            dic_not_end = dic_name
 
-                elif dic_not_end == "":
-                    zbb_func_dic[func_not_end]["end"] = public_var.line_num
-                    func_not_end = ""
+        if each_line.replace(" ", "").startswith("end") or each_line.startswith("END"):
+            if dic_not_end != "":
+                zbb_var_dic[dic_not_end] = temp_dic_
+                temp_dic_ = {}
+                dic_not_end = ""
 
-            if each_line.replace(" ","").startswith("item") or each_line.startswith("ITEM"):
-                item_keyword = "item" if each_line.replace(" ","").startswith("item") else "ITEM"
-                item_key = each_line.replace(item_keyword, '',1).replace(" ", '').split(",")[0]
-                item_value = each_line.replace(item_keyword, '', 1).replace(" ", '').split(",")[1]
-                temp_dic_[zbb_data(item_key)] = zbb_data(item_value)
+            elif dic_not_end == "":
+                zbb_func_dic[func_not_end]["end"] = public_var.line_num
+                func_not_end = ""
 
+        if each_line.replace(" ", "").startswith("item") or each_line.startswith("ITEM"):
+            item_keyword = "item" if each_line.replace(" ", "").startswith("item") else "ITEM"
+            item_key = each_line.replace(item_keyword, '', 1).replace(" ", '').split(",")[0]
+            item_value = each_line.replace(item_keyword, '', 1).replace(" ", '').split(",")[1]
+            temp_dic_[zbb_data(item_key)] = zbb_data(item_value)
+
+        public_var.line_num += 1
+
+    if dic_not_end != "":
+        print(f"{Fore.RED}Dic not end{Fore.RESET}")
+    if func_not_end != "":
+        print(f"{Fore.RED}Func not end{Fore.RESET}")
+
+    public_var.line_num = 0
+
+    """
+    阅读每一行zbb
+    """
+    while True:
+        if public_var.line_num < len(code_text_to_list):
+            each_line_of_code = code_text_to_list[public_var.line_num]
+        else:
+            break
+
+        if each_line_of_code == '' or each_line_of_code.replace(" ", "").startswith("//"):
             public_var.line_num += 1
+            continue
 
-        if dic_not_end != "" :
-            print(f"{Fore.RED}Dic not end{Fore.RESET}")
-        if func_not_end != "" :
-            print(f"{Fore.RED}Func not end{Fore.RESET}")
+        try:
+            change_line_num = run_a_line_of_zbb(each_line_of_code)
+            public_var.line_num = change_line_num if change_line_num is not None else public_var.line_num
+        except (RuntimeError, TypeError, KeyError) as e:
+            print(
+                f"{Fore.RED}{e}\n"
+                f"line {public_var.line_num + 1}: "
+                f"{code_text_to_list[public_var.line_num]} \n "
+                f"{" " * (6 + len(str(public_var.line_num)))}"
+                f"{'^' * len(code_text_to_list[public_var.line_num])}\n"
+                f"{Fore.RESET}"
+            )
+            break
 
-        public_var.line_num = 0
+        except Exception as e:
+            print(f"{Fore.RED}UnSupport error:,{e}{Fore.RESET}")
 
-        """
-        阅读每一行zbb
-        """
-        while True:
-            if public_var.line_num < len(code_text_to_list):
-                each_line_of_code = code_text_to_list[public_var.line_num]
-            else:
-                break
+        public_var.line_num += 1
 
-            if each_line_of_code == '' or each_line_of_code.replace(" ","").startswith("//"):
-                public_var.line_num += 1
-                continue
+        if public_var.line_num - 1 == len(code_text_to_list):
+            break
 
-            try:
-                change_line_num = run_a_line_of_zbb(each_line_of_code)
-                public_var.line_num = change_line_num if change_line_num is not None else public_var.line_num
-            except (RuntimeError, TypeError, KeyError) as e:
-                print(
-                    f"{Fore.RED}{e}\n"
-                    f"line {public_var.line_num + 1}: "
-                    f"{code_text_to_list[public_var.line_num]} \n "
-                    f"{" " * (6 + len(str(public_var.line_num))) }"
-                    f"{'^' * len(code_text_to_list[public_var.line_num])}\n"
-                    f"{Fore.RESET}"
-                )
-                break
-
-            except Exception as e:
-                print(f"{Fore.RED}UnSupport error:,{e}{Fore.RESET}")
-
-            public_var.line_num += 1
-
-            if public_var.line_num - 1 == len(code_text_to_list):
-                break
-
-        end_time = time.time()
-        print(f"\n==program done\nruntime:{end_time - start_time}==")
+    end_time = time.time()
+    print(f"\n==program done\nruntime:{end_time - start_time}==")
--- run.py
+++ run.py
@@ -13,12 +13,12 @@ if __name__ == "__main__":
                 break
 
         print("==欢迎使用ZBB解析器==\n"
-            "------------------\n"
-            "请输入指令\n"
-            "help 帮助\n"
-            "run <文件名> 运行文件\n"
-            "out 退出\n"
-            ,end="")
+              "------------------\n"
+              "请输入指令\n"
+              "help 帮助\n"
+              "run <文件名> 运行文件\n"
+              "out 退出"
+             )
         user_input = input(">>>")
         if user_input == "help":
             with open("documents/zbb_help.md", 'r', encoding='utf-8') as f:
@@ -40,12 +40,11 @@ if __name__ == "__main__":
                 print(f"{e}")
             except FileNotFoundError:
                 print(f"{Fore.RED}File '{zbb_file_to_run}' not found{Fore.RESET}")
-            finally:
-                input_to_exit = input(f"{Fore.GREEN}Type ENTER to continue{Fore.RESET}")
-                if input_to_exit == "":
-                    continue
-                else:
-                    break
+            input_to_exit = input(f"{Fore.GREEN}Type ENTER to continue{Fore.RESET}")
+            if input_to_exit == "":
+                continue
+            else:
+                break
         else:
             print(f"{Fore.RED}Not have introduction named {user_input}{Fore.RESET}")
             input_to_exit = input(f"{Fore.GREEN}Type ENTER to continue{Fore.RESET}")
